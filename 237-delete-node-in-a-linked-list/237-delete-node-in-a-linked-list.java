e1e1634442a2767643b79835f010cd2ee755e037
susmitha <73429559+susmitha2002@users.noreply.github.com>
2022-06-28 12:13:18 +0530

    Update 237-delete-node-in-a-linked-list.java

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
@@ -13,7 +13,7 @@ class Solution {
         node.next.val=node.val;
         node.val =tempval;
         // now we have access of prev node, current node, next node
-        // remove link for node to be deleted. (node.next is node to be deleted after                 swapping)
+        // remove link for node to be deleted. (node.next is node to be deleted after swapping)
         node.next= node.next.next;
     }
 }
