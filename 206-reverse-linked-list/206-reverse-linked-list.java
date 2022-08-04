272cb1361b6c8a22281d1ea2bdf3a5611700c42c
susmitha <73429559+susmitha2002@users.noreply.github.com>
2022-08-04 23:44:36 +0530

    Time: 0 ms (100.00%), Space: 43 MB (51.08%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,20 +9,20 @@
  * }
  */
 class Solution {
+    ListNode headnode = null;
+    public ListNode reverse(ListNode h){
+        if(h == null || h.next == null ){
+            headnode = h;
+            return h;
+        }
+        reverse(h.next);
+        h.next.next =  h;
+        // h.next = null is important bcuz for lastnode next must be null right? so that is needed or else after reversing 1st node and 2nd node keep on pointinfg each one soo cycle forms
+        h.next = null;
+        // System.out.println("headnode val: "+headnode.val+"h val: "+h.val);
+        return headnode;
+    }
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next ==null){
-            return head;
-        }
-        ListNode prev = null;
-        ListNode current = head;
-        ListNode nextnode =  head.next;
-        while(current != null){
-            current.next = prev;
-            prev= current;
-            current = nextnode;
-            if(nextnode !=null) nextnode = nextnode.next;
-        }
-        return prev;
-        
+        return reverse(head);
     }
 }
